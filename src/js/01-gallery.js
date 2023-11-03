1cf4dbe9d179d1f5c2ed865bcacd47766682f2ac
AgnieszkaPyzik <agnieszka.piescik@gmail.com>
2023-11-03 23:44:03 +0100

    Video Commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,23 @@
+import _default from '../../node_modules/simplelightbox/dist/simple-lightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+
+const photo = galleryItems
+  .map(
+    galleryItem =>
+      `<div class="gallery__item"> <a class="gallery__link" href="${galleryItem.original}"> <img class="gallery__image" src="${galleryItem.preview}" data-source="${galleryItem.original}" alt="${galleryItem.description}"/></a> </div>`
+  )
+  .join('');
+gallery.insertAdjacentHTML('beforeend', photo);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 300,
+});
+console.log(lightbox);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+'use strict';
 
+const KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+
+const player = new Vimeo(iframe);
+
+player.on('loaded', () => {
+  const currentTime = localStorage.getItem(KEY) || 0;
+  player.setCurrentTime(currentTime);
+});
+
+player.on(
+  'timeupdate',
+  throttle(({ seconds }) => {
+    localStorage.setItem(KEY, seconds);
+  }, 1000)
+);
