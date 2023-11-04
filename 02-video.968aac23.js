c6293e2ed3b034b5a6b1e6ca06f9a9b7b4d23b54
AgnieszkaPyzik <126672978+AgnieszkaPyzik@users.noreply.github.com>
2023-11-04 22:20:01 +0000

    Deploying to gh-pages from @ AgnieszkaPyzik/goit-js-hw-08@e5eac9656116a26c2151351d2d98bde1def1ccd6 🚀

--- 02-video.968aac23.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("iframe"),t=new Vimeo(e);t.on("loaded",(()=>{const e=localStorage.getItem("videoplayer-current-time")||0;t.setCurrentTime(e)})),t.on("timeupdate",throttle((({seconds:e})=>{localStorage.setItem("videoplayer-current-time",e)}),1e3));
-//# sourceMappingURL=02-video.968aac23.js.map
